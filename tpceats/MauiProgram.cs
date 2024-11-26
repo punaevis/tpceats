57141d57e9d1369e36b145eebe14e2dba9d7b178
Your Name <your_email@example.com>
2024-11-26 12:06:30 +0700

    Add project files.

---
+++ tpceats/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace tpceats;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("FunnelSans-Bold.ttf", "MainFont");
+                fonts.AddFont("FunnelSans-BoldItalic.ttf", "MainFontItalic");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
